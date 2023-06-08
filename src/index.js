4ab3a9cd8b8288ffffc0b7b85b5463aab3a82b41
MatiaPercic <mapercic@unipu.hr>
2023-06-08 18:03:10 +0200

    index

--- src/index.js
+++ src/index.js
@@ -1,14 +1,38 @@
 import express  from "express";
+import cors from "cors";
 
 const app=express()
-const port=3000
+const port=3001
 
-app.get('/',(req,res) =>{
-    console.log("working")
-    res.send('Hello world');
-})
+app.use(express.json());
+app.use(cors());
 
+app.get("/volonteri",(req,res)=>{
+    console.log("working");
+
+    let volonteri= [
+
+        {
+            ime:'Ivo',
+            prezime: 'Ivić',
+            email:'iivić@gmail.com',
+            godine: '22',
+            br_aktivnosti: '5',
+            br_sati:'12'
+        },
+
+        {
+            ime:'Marko',
+            prezime: 'Marković',
+            email:'mmarković@gmail.com',
+            godine:'21',
+            br_aktivnosti:'2',
+            br_sati:'4'
+        }
+    ];
+    res.send(volonteri);
+});
 
 app.listen(port,()=>{
-    console.log('app listening on port',port);
+    console.log('listening on port ',port);
 })

31661442fbe693a938f35e34c35391c8e4a12ecb
MatiaPercic <mapercic@unipu.hr>
2023-06-09 14:27:27 +0200

    working login on fr without db connection

--- src/index.js
+++ src/index.js
@@ -1,38 +1,55 @@
-import express  from "express";
+import express from "express";
 import cors from "cors";
 
-const app=express()
-const port=3001
+const app = express();
+const port = 3001;
+
+const adminUser = {
+  ime: 'Matia',
+  prezime: 'Perčić',
+  email: 'mpercic@gmail.com',
+  password: 'percic'
+};
 
 app.use(express.json());
 app.use(cors());
 
-app.get("/volonteri",(req,res)=>{
-    console.log("working");
+app.get("/volonteri", (req, res) => {
+  console.log("working");
 
-    let volonteri= [
-
-        {
-            ime:'Ivo',
-            prezime: 'Ivić',
-            email:'iivić@gmail.com',
-            godine: '22',
-            br_aktivnosti: '5',
-            br_sati:'12'
-        },
-
-        {
-            ime:'Marko',
-            prezime: 'Marković',
-            email:'mmarković@gmail.com',
-            godine:'21',
-            br_aktivnosti:'2',
-            br_sati:'4'
-        }
-    ];
-    res.send(volonteri);
+  let volonteri = [
+    {
+      ime: 'Ivo',
+      prezime: 'Ivić',
+      email: 'iivić@gmail.com',
+      godine: 22,
+      br_aktivnosti: 5,
+      br_sati: 12
+    },
+    {
+      ime: 'Marko',
+      prezime: 'Marković',
+      email: 'mmarković@gmail.com',
+      godine: 21,
+      br_aktivnosti: 2,
+      br_sati: 4
+    }
+  ];
+  
+  res.send(volonteri);
 });
 
-app.listen(port,()=>{
-    console.log('listening on port ',port);
-})
+app.post("/login", (req, res) => {
+  console.log("posting");
+
+  let workingLogin = false;
+
+  if (req.body.email === adminUser.email && req.body.password === adminUser.password)
+    workingLogin = true;
+
+  res.send(workingLogin);
+});
+
+app.listen(port, () => {
+  console.log('listening on port', port);
+});

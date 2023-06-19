c2478e3d06bc60082983d4a678272aab879deb95
MatiaPercic <mapercic@unipu.hr>
2023-06-19 11:30:28 +0200

    broj sati i akt volontera u ovoj i prosloj godini

--- src/index.js
+++ src/index.js
@@ -8,6 +8,10 @@ const port = 3001;
 app.use(express.json());
 app.use(cors());
 
+
+
+
+
 // -----LOGIN -----
 
 app.post("/login", async (req, res) => {
@@ -35,6 +39,9 @@ app.post("/login", async (req, res) => {
   res.send(volonter);
 });
 
+
+
+
 // ----- loginAdmin -----
 
 app.post("/loginAdmin", async (req, res) => {
@@ -61,6 +68,9 @@ app.post("/loginAdmin", async (req, res) => {
   res.status(201);
   res.send(admin);
 });
+
+
+
 
 // REGISTER
 
@@ -103,6 +113,9 @@ app.post("/register", async (req, res) => {
   }
 });
 
+
+
+
 // -----register admin-----
 
 app.post("/registerAdmin", async (req, res) => {
@@ -143,6 +156,10 @@ app.post("/registerAdmin", async (req, res) => {
   }
 });
 
+
+
+
+
 // ----- find volonter by email-----
 app.post("/volonterInfo", async (req, res) => {
   let db = await connectDB();
@@ -165,6 +182,9 @@ app.post("/volonterInfo", async (req, res) => {
   res.status(201);
   res.send(volonter);
 });
+
+
+
 
 // -----update volonter -----
 
@@ -240,7 +260,6 @@ app.put("/updateAdmin", async (req, res) => {
 
 
 
-
 // ----- Prikaz svih  aktivnosti -----
 
 app.post("/aktivnostiAll", async (req, res) => {
@@ -261,15 +280,58 @@ app.post("/aktivnostiAll", async (req, res) => {
     },
   };
 
-  let sort={
-    datum:-1
+  let sort = {
+    datum: -1,
   };
 
-
-  let aktivnosti = await aktivnostiCol.find({}, projection,).sort(sort).toArray();
+  let aktivnosti = await aktivnostiCol
+    .find({}, projection)
+    .sort(sort)
+    .toArray();
 
   res.send(aktivnosti);
 });
+
+
+
+
+
+//--- aktivnosti po volonteru
+
+app.post("/aktivnostiVolonter", async (req, res) => {
+  let db = await connectDB();
+  let aktivnostiCol = db.collection("Aktivnosti");
+
+  let vol_email = req.body.email;
+
+  const pipeline = [
+    {
+      $match: {
+        volonteri: { $in: [vol_email] },
+      },
+    },
+    {
+      $sort: {
+        datum: -1,
+      },
+    },
+    {
+      $project: {
+        datum: { $dateToString: { format: "%Y-%m-%d", date: "$datum" } },
+        opis: 1,
+        oblik_rada: 1,
+        volonteri: 1,
+        admin: 1,
+        sati: 1,
+      },
+    },
+  ];
+
+  const result = await aktivnostiCol.aggregate(pipeline).toArray();
+
+  res.send(result);
+});
+
 
 
 
@@ -278,7 +340,7 @@ app.post("/aktivnostiAll", async (req, res) => {
 app.put("/updateSati", async (req, res) => {
   const db = await connectDB();
   let aktivnostiCol = db.collection("Aktivnosti");
-  let volonteri=db.collection("Volonteri");
+  let volonteri = db.collection("Volonteri");
 
   let vol_email = req.body.email;
 
@@ -298,36 +360,30 @@ app.put("/updateSati", async (req, res) => {
   for (let akt of aktivnosti) {
     br_akt += 1;
     br_sati += akt.sati;
-    update=true;
+    update = true;
   }
 
-  let promjene={
-    $set:{
-      broj_volonterskih_sati:br_sati,
-      broj_aktivnosti:br_akt
-
-    }
+  let promjene = {
+    $set: {
+      broj_volonterskih_sati: br_sati,
+      broj_aktivnosti: br_akt,
+    },
   };
 
-
-
-  let vol_query={
-    email:vol_email
+  let vol_query = {
+    email: vol_email,
   };
 
+  let volonter = await volonteri.findOne(vol_query);
 
-
-  let volonter= await volonteri.findOne(vol_query);
-
-if (volonter) {
+  if (volonter) {
     let updateVolonter = await volonteri.updateOne(vol_query, promjene);
 
     console.log("Promijenjeni osobni podaci volontera");
     update = true;
   } else console.log("greška pri izmijeni podataka");
 
-
-  let volonter2= await volonteri.findOne(vol_query);
+  let volonter2 = await volonteri.findOne(vol_query);
   console.log(br_akt);
   console.log(br_sati);
 
@@ -335,42 +391,154 @@ if (volonter) {
 });
 
 
+//----- prikaz sati i aktivnosti volontera u ovoj godini
 
-//--- aktivnosti po volonteru
-
-app.post("/aktivnostiVolonter", async (req, res) => {
+app.post("/volonterCurrentYear", async (req, res) => {
   let db = await connectDB();
-  let aktivnostiCol = db.collection("Aktivnosti");
+  let aktivnosti = db.collection("Aktivnosti");
+  let volonteri = db.collection("Volonteri");
+
+  let volonter = {
+    ime: "",
+    prezime: "",
+    godine: "",
+    email: req.body.email,
+    broj_aktivnosti: 0,
+    broj_volonterskih_sati: 0,
+  };
+
+  let currentYear = new Date().getFullYear();
 
   let vol_email = req.body.email;
 
-  const pipeline = [
+  let vol_q = {
+    email: req.body.email,
+  };
+
+  let vol_p = {
+    projection: {
+      _id: 0,
+      ime: 1,
+      prezime: 1,
+      godine: 1,
+      email: 1,
+      broj_aktivnosti: 1,
+      broj_volonterskih_sati: 1,
+    },
+  };
+
+  let vol = await volonteri.findOne(vol_q, vol_p);
+
+  if (vol) volonter = vol;
+
+  let pipeline = [
     {
       $match: {
         volonteri: { $in: [vol_email] },
+        datum: {
+          $gte: new Date(currentYear, 0, 1),
+          $lt: new Date(currentYear + 1, 0, 1),
+        },
       },
     },
-    {
-      $sort:{
-        datum:-1,
-      }
-    },
-    {
-      $project:{
-        datum: { $dateToString: { format: "%Y-%m-%d", date: "$datum" } },
-        opis: 1,
-        oblik_rada: 1,
-        volonteri: 1,
-        admin: 1,
-        sati: 1,
-      }
-    }
   ];
 
-  const result = await aktivnostiCol.aggregate(pipeline).toArray();
+  let aktivnost = await aktivnosti.aggregate(pipeline).toArray();
 
-  res.send(result);
+  let br_akt = 0;
+  let br_sati = 0;
+  let update = false;
+
+  for (let akt of aktivnost) {
+    br_akt += 1;
+    br_sati += akt.sati;
+    update = true;
+  }
+
+  volonter.broj_aktivnosti = br_akt;
+  volonter.broj_volonterskih_sati = br_sati;
+
+  res.send(volonter);
 });
+
+
+
+
+
+// ----- prikaz sati i aktivnosti volontera i prošloj godini-----
+
+
+app.post("/volonterLastYear", async (req, res) => {
+  let db = await connectDB();
+  let aktivnosti = db.collection("Aktivnosti");
+  let volonteri = db.collection("Volonteri");
+
+  let volonter = {
+    ime: "",
+    prezime: "",
+    godine: "",
+    email: req.body.email,
+    broj_aktivnosti: 0,
+    broj_volonterskih_sati: 0,
+  };
+
+  let lastYear = new Date().getFullYear() -1;
+
+  let vol_email = req.body.email;
+
+  let vol_q = {
+    email: req.body.email,
+  };
+
+  let vol_p = {
+    projection: {
+      _id: 0,
+      ime: 1,
+      prezime: 1,
+      godine: 1,
+      email: 1,
+      broj_aktivnosti: 1,
+      broj_volonterskih_sati: 1,
+    },
+  };
+
+  let vol = await volonteri.findOne(vol_q, vol_p);
+
+  if (vol) volonter = vol;
+
+  let pipeline = [
+    {
+      $match: {
+        volonteri: { $in: [vol_email] },
+        datum: {
+          $gte: new Date(lastYear, 0, 1),
+          $lt: new Date(lastYear + 1, 0, 1),
+        },
+      },
+    },
+  ];
+
+  let aktivnost = await aktivnosti.aggregate(pipeline).toArray();
+
+  let br_akt = 0;
+  let br_sati = 0;
+  let update = false;
+
+  for (let akt of aktivnost) {
+    br_akt += 1;
+    br_sati += akt.sati;
+    update = true;
+  }
+
+  volonter.broj_aktivnosti = br_akt;
+  volonter.broj_volonterskih_sati = br_sati;
+
+  res.send(volonter);
+});
+
+
+
+
 app.listen(port, () => {
   console.log("listening on port", port);
 });
